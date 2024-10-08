aab15b4cf8c6a673ecf3f65678c3a43f2c713dc8
Lee Artem <fcknawsm@ya.ru>
2024-10-08 19:41:10 +0900

    fixes

--- up.sh
+++ up.sh
@@ -1,3 +1,4 @@
 docker compose up -d \
   && docker compose run --rm composer i \
-  && docker compose run --rm artisan migrate:fresh --seed
+  && docker compose run --rm artisan migrate:fresh --seed \
+  && echo "Preparing is done ✅"
